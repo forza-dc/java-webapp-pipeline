c2741d19dd303de7baa72ddfb88794daf23446b8
S T <stsamdc@hotmail.com>
2024-06-21 12:39:32 +0000

    Updated Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -9,4 +9,9 @@ public class Demo {
 
         System.out.println("I am not covered");
     }
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.DoSomething(true); // Or false, based on your test
+    }
 }
