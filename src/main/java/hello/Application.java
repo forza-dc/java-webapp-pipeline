bb67049bb445cadfb77054cd37f50861e2c6b41e
S T <stsamdc@hotmail.com>
2024-06-21 17:27:35 +0000

    Updated Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World ! ";
+		return "Welcome to Forza App ! ";
 	}
 
 	public static void main(String[] args) {
