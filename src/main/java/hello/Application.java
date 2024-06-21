2d0e554afa10b516256ba3f204e0b7586c7b98ee
S T <stsamdc@hotmail.com>
2024-06-21 20:51:24 +0000

    Updated Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,20 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Welcome to Forza App ! ";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
